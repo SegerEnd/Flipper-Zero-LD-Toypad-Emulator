f1abe33f2d3eaa6068da956cc10edcd6b908774c
SegerEnd <gj.endendijk@gmail.com>
2025-01-05 23:13:22 +0100

    Revert "Toypad is being detected by the game | Merge pull request #1 from SegerEnd/dev"
    This reverts commit fa19abc903f7c531cdfe3d68a164cb7ba088ca1a, reversing
    changes made to a2ff9450481df4512f8ac2a0fda6a3a352ef9d07.

--- usb/usb_toypad.h
+++ usb/usb_toypad.h
@@ -1,65 +1,10 @@
 #pragma once
 #include <furi.h>
 #include <furi_hal.h>
-#include <pthread.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-#define HID_EP_SZ 0x20
-
-typedef struct {
-    int index;
-    int id;
-    int pad;
-    char uid[7];
-    // unsigned char data[180];
-    char token[32];
-} Token;
-
-typedef struct {
-    Token tokens[10];
-    int token_count;
-    void (*transport_write)(const char* data);
-    uint8_t tea_key[16];
-} ToyPadEmu;
-
-typedef struct {
-    unsigned char type;
-    unsigned char len;
-    unsigned char payload[HID_EP_SZ - 2];
-    unsigned char chksum;
-} Frame;
-
-// Define a Response structure
-typedef struct {
-    Frame frame;
-    unsigned char cid;
-    unsigned char payload[HID_EP_SZ];
-    int payload_len;
-    // int _cancel;
-    // int _preventDefault;
-} Response;
-
-// Define a Request structure
-typedef struct {
-    Frame frame;
-    unsigned char cmd;
-    unsigned char cid;
-    unsigned char payload[HID_EP_SZ - 2];
-    int payload_len;
-} Request;
-
-// Event structure
-
-typedef struct {
-    Frame frame;
-    int pad;
-    int index;
-    int dir;
-    int uid[7];
-} Event;
 
 extern FuriHalUsbInterface usb_hid_ldtoypad;
 
@@ -75,30 +20,6 @@ int32_t hid_toypad_read_IN();
 
 char* get_debug_text_ep_in();
 char* get_debug_text_ep_out();
-
-char* get_debug_text();
-
-void set_debug_text(char* text);
-
-usbd_device* get_usb_device();
-
-void ToyPadEmu_randomUID(unsigned char* uid);
-
-Token createCharacter(int id, unsigned char* uid);
-
-void ToyPadEmu_place(ToyPadEmu* emu, int pad, int index, unsigned char* uid);
-
-void Event_init(Event* event);
-
-int Event_build(Event* event, unsigned char* buf);
-
-int build_frame(Frame* frame, unsigned char* buf);
-
-int build_response(Response* response, unsigned char* buf);
-
-ToyPadEmu* get_emulator();
-
-bool get_connected_status();
 
 #ifdef __cplusplus
 }
